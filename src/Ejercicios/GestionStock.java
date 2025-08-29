c911c24f6b4412479f437efb69d0a67cdf1824fa
Alep9923 <alepalma9923@gmail.com>
2025-08-29 10:19:06 -0500

    Actualización: Entrega de ejercicio 2

--- src/Ejercicios/GestionStock.java
+++ src/Ejercicios/GestionStock.java
@@ -1,4 +1,30 @@
 package Ejercicios;
 
 public class GestionStock {
+    private int stock;
+
+
+    public void agregarStock(int cantidad) {
+        if (cantidad <= 0) {
+            throw new IllegalArgumentException("La cantidad a agregar debe ser positiva");
+        }
+        stock += cantidad;
+    }
+
+
+    public void reducirStock(int cantidad) {
+        if (cantidad <= 0) {
+            throw new IllegalArgumentException("La cantidad a reducir debe ser positiva");
+        }
+        if (cantidad > stock) {
+            throw new IllegalArgumentException("No hay suficiente stock disponible. Stock actual: " + stock);
+        }
+        stock -= cantidad;
+    }
+
+
+
+    public int getStock() {
+        return stock;
+    }
 }
